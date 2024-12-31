0bc288b92f6bb00697c8d8ede18fa7a300790440
BashPrime <5769306+BashPrime@users.noreply.github.com>
2024-12-31 14:59:19 -0800

    adjust electron window size

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import { isDev } from './util.js'
 
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({ width: 1152, height: 854 })
+    const mainWindow = new BrowserWindow({ width: 1280, height: 833 })
     if (isDev()) {
         mainWindow.loadURL('http://localhost:5173')
     } else {
