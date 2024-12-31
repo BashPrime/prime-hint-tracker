bb0e8eb926c8232b2b2d880bb1a9ba0704a01b42
BashPrime <5769306+BashPrime@users.noreply.github.com>
2024-12-30 22:35:51 -0800

    cleanup

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-import { cn } from './lib/utils'
 import ItemList from './views/ItemList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <ItemList />
