a35087ec2e5457ba8bc27bb8dd253cd45f2784e8
BashPrime <5769306+BashPrime@users.noreply.github.com>
2024-12-30 23:47:07 -0800

    hint state, label props

--- src/ui/views/ItemLocationHint.tsx
+++ src/ui/views/ItemLocationHint.tsx
@@ -1,24 +1,35 @@
 import { useState } from 'react'
 import { Input } from '../components/ui/input'
+import { cn } from '../lib/utils'
 
-export default function ItemLocationHintI() {
-  const [item, setItem] = useState<string>('')
-  const [location, setLocation] = useState<string>('')
+type Props = {
+  label?: string
+}
+
+export default function ItemLocationHint(props: Props) {
+  const [label, setLabel] = useState<string>('')
+  const [value, setValue] = useState<string>('')
 
   return (
     <>
+      {props.label && (
+        <p className={cn('font-semibold')}>{props.label}</p>
+      )}
+      {!props.label && (
+        <Input
+          type="text"
+          value={label}
+          onChange={(event) => {
+            setLabel(event.target.value)
+          }}
+          className={cn('font-semibold border-none')}
+        />
+      )}
       <Input
         type="text"
-        value={item}
+        value={value}
         onChange={(event) => {
-          setItem(event.target.value)
-        }}
-      />
-      <Input
-        type="text"
-        value={location}
-        onChange={(event) => {
-          setLocation(event.target.value)
+          setValue(event.target.value)
         }}
       />
     </>
--- src/ui/views/Prime2Layout.tsx
+++ src/ui/views/Prime2Layout.tsx
@@ -1,17 +1,17 @@
 import ItemLocationHint from "./ItemLocationHint"
 
 export default function Prime2Layout() {
-    return (
-        <div>
-            <ItemLocationHint />
-            <p>Items</p>
-            <p>STKs</p>
-            <p>Violet hints</p>
-            <p>Amber hints</p>
-            <p>Emerald hints</p>
-            <p>Cobalt hints</p>
-            <p>Keybearer hints</p>
-            <p>Bosses and Temple Keys</p>
-        </div>
-    )
+  return (
+    <>
+      <ItemLocationHint label="Dark Beam" />
+      <p>Items</p>
+      <p>STKs</p>
+      <p>Violet hints</p>
+      <p>Amber hints</p>
+      <p>Emerald hints</p>
+      <p>Cobalt hints</p>
+      <p>Keybearer hints</p>
+      <p>Bosses and Temple Keys</p>
+    </>
+  )
 }
