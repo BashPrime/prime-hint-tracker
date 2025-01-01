00ffa98b87e3953ac92804fce29f0e96368e8ff1
BashPrime <5769306+BashPrime@users.noreply.github.com>
2024-12-31 16:20:51 -0800

    custom menu with reset size logic

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,14 @@
-import {app, BrowserWindow} from 'electron'
-import path from 'path'
-import { isDev } from './util.js'
+import { app } from "electron";
+import path from "path";
+import { isDev } from "./util.js";
+import { create } from "./window.js";
 
-app.on('ready', () => {
-    const mainWindow = new BrowserWindow({ width: 1280, height: 833 })
-    if (isDev()) {
-        mainWindow.loadURL('http://localhost:5173')
-    } else {
-        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
-    }
-})
+
+app.on("ready", () => {
+  const mainWindow = create();
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
+});
---
+++ src/electron/menu.ts
@@ -0,0 +1,23 @@
+import { Menu, MenuItemConstructorOptions } from "electron";
+import { resetSize } from "./window.js";
+
+const isMac = process.platform === "darwin";
+const template: MenuItemConstructorOptions[] = [
+  {
+    label: "File",
+    submenu: [
+      { label: "Reset Size", click: () => resetSize() },
+      { label: "Reset Tracker" },
+      { role: isMac ? "close" : "quit" },
+    ],
+  },
+  { role: "editMenu" },
+  { role: "viewMenu" },
+  { role: "windowMenu" },
+  // {
+  //   label: "Game",
+  //   submenu: [{ label: "Metroid Prime", enabled: false }, { label: "Metroid Prime 2: Echoes" }],
+  // },
+];
+
+export const menu = Menu.buildFromTemplate(template);
