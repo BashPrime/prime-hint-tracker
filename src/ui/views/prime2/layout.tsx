89b2b116dac24a0b6273bf138f092da4baf98846
BashPrime <5769306+BashPrime@users.noreply.github.com>
2025-01-02 16:53:28 -0800

    use full names for region headers

--- src/ui/views/prime2/layout.tsx
+++ src/ui/views/prime2/layout.tsx
@@ -5,10 +5,10 @@ import Region from "./region";
 export default function Prime2Layout() {
   return (
     <div className={cn("grid sm:grid-cols-2 md:grid-cols-4 h-full bg-[#1f1f1f]")}>
-      <Region name="Temple" hints={templeGroundsHintsState} className="border-l-2 border-violet-600" />
-      <Region name="Agon" hints={agonHintsState} className="border-l-2 border-amber-600" />
-      <Region name="Torvus" hints={torvusHintsState} className="border-l-2 border-emerald-600" />
-      <Region name="Sanctuary" hints={sanctuaryHintsState} className="border-l-2 border-sky-600" />
+      <Region name="Temple Grounds" hints={templeGroundsHintsState} className="border-l-2 border-violet-600" />
+      <Region name="Agon Wastes" hints={agonHintsState} className="border-l-2 border-amber-600" />
+      <Region name="Torvus Bog" hints={torvusHintsState} className="border-l-2 border-emerald-600" />
+      <Region name="Sanctuary Fortress" hints={sanctuaryHintsState} className="border-l-2 border-sky-600" />
     </div>
   );
 }
