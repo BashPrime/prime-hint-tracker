5724251b9d88d12eb3482f5235c5705a91418b77
BashPrime <5769306+BashPrime@users.noreply.github.com>
2025-01-04 22:18:58 -0800

    wrap app root in jotai provider

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,11 +1,12 @@
+import { Provider } from 'jotai'
 import './App.css'
 import LayoutSelector from './views/LayoutSelector'
 
 export default function App() {
 
   return (
-    <>
+    <Provider>
       <LayoutSelector />
-    </>
+    </Provider>
   )
 }
