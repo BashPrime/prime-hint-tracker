be91c16a7aed26b8bdcfd8abd078913c5a9c0c80
BashPrime <5769306+BashPrime@users.noreply.github.com>
2025-01-06 17:16:13 -0800

    remove exit option from menu

--- src/electron/menu.ts
+++ src/electron/menu.ts
@@ -6,24 +6,18 @@ function resetTracker() {
   window.webContents.send("reset-tracker");
 }
 
-const isMac = process.platform === "darwin";
 const template: MenuItemConstructorOptions[] = [
   {
     label: "File",
     submenu: [
       { label: "Reset Size", click: () => resetSize() },
       { label: "Reset Tracker", click: () => resetTracker() },
-      { role: isMac ? "close" : "quit" },
     ],
   },
   { role: "editMenu" },
   { role: "viewMenu" },
   { role: "windowMenu" },
   { label: `Version ${app.getVersion()}` },
-  // {
-  //   label: "Game",
-  //   submenu: [{ label: "Metroid Prime", enabled: false }, { label: "Metroid Prime 2: Echoes" }],
-  // },
 ];
 
 export const menu = Menu.buildFromTemplate(template);
