22d90b4e2581dde55388448126182dd574a14580
BashPrime <5769306+BashPrime@users.noreply.github.com>
2025-04-04 01:33:11 -0700

    more parse improvements

--- src/ui/states/Prime1.states.ts
+++ src/ui/states/Prime1.states.ts
@@ -2,8 +2,8 @@ import { atom } from "jotai";
 import { atomWithReset } from "jotai/utils";
 import { unhintedItemsState } from "./App.states";
 import {
-  ArtifactHint,
-  ArtifactHintSchema,
+  ArtifactHints,
+  ArtifactHintsSchema,
   PhazonSuitHint,
 } from "@/types/Prime1.types";
 import { HintOption } from "@/types/common.types";
@@ -13,20 +13,22 @@ import {
   PRIME_1_MAJOR_UPGRADES,
 } from "@/data/Prime1.data";
 
-export const artifactHintsState = atomWithReset<ArtifactHint[]>([
-  ArtifactHintSchema.parse({ id: 1, name: "Truth" }),
-  ArtifactHintSchema.parse({ id: 2, name: "Strength" }),
-  ArtifactHintSchema.parse({ id: 3, name: "Elder" }),
-  ArtifactHintSchema.parse({ id: 4, name: "Wild" }),
-  ArtifactHintSchema.parse({ id: 5, name: "Lifegiver" }),
-  ArtifactHintSchema.parse({ id: 6, name: "Warrior" }),
-  ArtifactHintSchema.parse({ id: 7, name: "Chozo" }),
-  ArtifactHintSchema.parse({ id: 8, name: "Nature" }),
-  ArtifactHintSchema.parse({ id: 9, name: "Sun" }),
-  ArtifactHintSchema.parse({ id: 10, name: "World" }),
-  ArtifactHintSchema.parse({ id: 11, name: "Spirit" }),
-  ArtifactHintSchema.parse({ id: 12, name: "Newborn" }),
-]);
+export const artifactHintsState = atomWithReset<ArtifactHints>(
+  ArtifactHintsSchema.parse([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 },
+    { id: 11 },
+    { id: 12 },
+  ])
+);
 
 export const phazonSuitHintState = atomWithReset<PhazonSuitHint>({
   location: "",
--- src/ui/types/Prime1.types.ts
+++ src/ui/types/Prime1.types.ts
@@ -8,79 +8,78 @@ export const ArtifactHintSchema = z.object({
 });
 export type ArtifactHint = z.infer<typeof ArtifactHintSchema>;
 
-
 export const ArtifactHintsSchema = z.tuple([
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Truth").catch("Truth"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Strength").catch("Strength"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Elder").catch("Elder"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Wild").catch("Wild"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Lifegiver").catch("Lifegiver"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Warrior").catch("Warrior"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Chozo").catch("Chozo"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Nature").catch("Nature"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Sun").catch("Sun"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("World").catch("World"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Spirit").catch("Spirit"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
   z.object({
     id: ArtifactHintSchema.shape.id,
     name: z.literal("Newborn").catch("Newborn"),
     location: ArtifactHintSchema.shape.location,
-    checked: ArtifactHintSchema.shape.checked
+    checked: ArtifactHintSchema.shape.checked,
   }),
 ]);
 export type ArtifactHints = z.infer<typeof ArtifactHintsSchema>;
