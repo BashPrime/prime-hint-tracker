df6b5e56d68c3fd49c52b8912919b4a30f0620f9
BashPrime <5769306+BashPrime@users.noreply.github.com>
2025-04-04 17:47:18 -0700

    more cleanup

--- src/ui/types/Prime1.types.ts
+++ src/ui/types/Prime1.types.ts
@@ -8,20 +8,20 @@ export const ArtifactHintSchema = z.object({
 export type ArtifactHint = z.infer<typeof ArtifactHintSchema>;
 
 export const ArtifactHintsSchema = z.object({
-  "Truth": ArtifactHintSchema,
-  "Strength": ArtifactHintSchema,
-  "Elder": ArtifactHintSchema,
-  "Wild": ArtifactHintSchema,
-  "Lifegiver": ArtifactHintSchema,
-  "Warrior": ArtifactHintSchema,
-  "Chozo": ArtifactHintSchema,
-  "Nature": ArtifactHintSchema,
-  "Sun": ArtifactHintSchema,
-  "World": ArtifactHintSchema,
-  "Spirit": ArtifactHintSchema,
-  "Newborn": ArtifactHintSchema,
-})
-export type ArtifactHints = z.infer<typeof ArtifactHintsSchema>
+  Truth: ArtifactHintSchema,
+  Strength: ArtifactHintSchema,
+  Elder: ArtifactHintSchema,
+  Wild: ArtifactHintSchema,
+  Lifegiver: ArtifactHintSchema,
+  Warrior: ArtifactHintSchema,
+  Chozo: ArtifactHintSchema,
+  Nature: ArtifactHintSchema,
+  Sun: ArtifactHintSchema,
+  World: ArtifactHintSchema,
+  Spirit: ArtifactHintSchema,
+  Newborn: ArtifactHintSchema,
+});
+export type ArtifactHints = z.infer<typeof ArtifactHintsSchema>;
 
 export const PhazonSuitHintSchema = z.object({
   location: z.string().default(""),
